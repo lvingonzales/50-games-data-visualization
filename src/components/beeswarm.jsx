01cd21ef91b125e1b3ae7cdcbafdf75cd596c57e
lvingonzales <lvingonzales15@gmail.com>
2025-02-07 14:39:46 -0500

    Fixed beeswarm plot criteria

--- src/components/beeswarm.jsx
+++ src/components/beeswarm.jsx
@@ -5,6 +5,80 @@ import style_treeMap from "../styles/style_treemap.module.css";
 export default function BeeSwarm({ colours, activeChart, activeCriteria }) {
   const chartRef = useRef(null);
 
+  const renderChart = () => {
+    const chartDimensions = {
+      width: 1000,
+      height: 300,
+    };
+
+    const margins = {
+      right: 20,
+      left: 20,
+      bottom: 20,
+    };
+
+    const radius = 10;
+    const padding = 2;
+
+    d3.select(chartRef.current).html('');
+
+    d3.csv("/Games.nonHier.csv").then((data) => {
+      console.log("Raw Data: ", data);
+      const x = d3
+        .scaleLinear()
+        .domain(d3.extent(data, (datum) => {
+          console.log(datum[toString(activeCriteria)]);
+          return datum[activeCriteria]
+        }))
+        .range([margins.left, chartDimensions.width - margins.right]);
+
+      const svg = d3
+        .select(chartRef.current)
+        .append("svg")
+        .attr("width", chartDimensions.width)
+        .attr("height", chartDimensions.height)
+        .attr("viewBox", [0, 0, chartDimensions.width, chartDimensions.height])
+        .attr(
+          "style",
+          "max-width: 100%; height: auto; border: 1px solid black;"
+        );
+
+      svg
+        .append("g")
+        .attr(
+          "transform",
+          `translate(0, ${chartDimensions.height - margins.bottom})`
+        )
+        .call(d3.axisBottom(x).tickSizeOuter(0).tickFormat(d3.format("d")));
+
+      svg
+        .append("g")
+        .attr("transform", `translate(0, 125)`)
+        .attr("style", "border: 1px solid black;")
+        .selectAll("circle")
+        .data(
+          dodge(data, {
+            radius: radius * 2 + padding,
+            x: (datum) => x(datum[activeCriteria]),
+          })
+        )
+        .join("circle")
+        .attr("cx", (datum) => datum.x)
+        .attr(
+          "cy",
+          (datum) => chartDimensions.height / 2 - radius - padding - datum.y
+        )
+        .attr("r", radius)
+        .attr("fill", (datum) => {
+          let genre = datum.data.Genre;
+
+          return colours[genre ? genre : "default"];
+        })
+        .append("title")
+        .text((datum) => datum.data.Game);
+    });
+  }
+
   useEffect(() => {
     if (activeChart === "bee") {
       chartRef.current.classList.add(style_treeMap.activeChart);
@@ -55,75 +129,7 @@ export default function BeeSwarm({ colours, activeChart, activeCriteria }) {
   };
 
   useEffect(() => {
-    const chartDimensions = {
-      width: 1000,
-      height: 200,
-    };
-
-    const margins = {
-      right: 20,
-      left: 20,
-      bottom: 20,
-    };
-
-    const radius = 10;
-    const padding = 2;
-
-    d3.csv("/Games.nonHier.csv").then((data) => {
-      console.log("Raw Data: ", data);
-      const x = d3
-        .scaleLinear()
-        .domain(d3.extent(data, (datum) => {
-          console.log(datum[toString(activeCriteria)]);
-          return datum[activeCriteria]
-        }))
-        .range([margins.left, chartDimensions.width - margins.right]);
-
-      const svg = d3
-        .select(chartRef.current)
-        .append("svg")
-        .attr("width", chartDimensions.width)
-        .attr("height", chartDimensions.height)
-        .attr("viewBox", [0, 0, chartDimensions.width, chartDimensions.height])
-        .attr(
-          "style",
-          "max-width: 100%; height: auto; border: 1px solid black;"
-        );
-
-      svg
-        .append("g")
-        .attr(
-          "transform",
-          `translate(0, ${chartDimensions.height - margins.bottom})`
-        )
-        .call(d3.axisBottom(x).tickSizeOuter(0).tickFormat(d3.format("d")));
-
-      svg
-        .append("g")
-        .attr("transform", `translate(0, 75)`)
-        .attr("style", "border: 1px solid black;")
-        .selectAll("circle")
-        .data(
-          dodge(data, {
-            radius: radius * 2 + padding,
-            x: (datum) => x(datum[activeCriteria]),
-          })
-        )
-        .join("circle")
-        .attr("cx", (datum) => datum.x)
-        .attr(
-          "cy",
-          (datum) => chartDimensions.height / 2 - radius - padding - datum.y
-        )
-        .attr("r", radius)
-        .attr("fill", (datum) => {
-          let genre = datum.data.Genre;
-
-          return colours[genre ? genre : "default"];
-        })
-        .append("title")
-        .text((datum) => datum.data.Game);
-    });
+    renderChart();
   }, [activeCriteria]);
 
   return <div className={style_treeMap.swarmWrapper} ref={chartRef}></div>;
